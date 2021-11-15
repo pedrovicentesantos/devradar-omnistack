6748be8768a1587d905c6f3df2b7eba25fb2afb1
Pedro Vicente Santos <pedrovicente@poli.ufrj.br>
2021-11-15 17:08:36 -0300

    feat: Receive search radius as parameter

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -5,12 +5,13 @@ module.exports = {
   async index(request,response){
     // Buscar todos os devs num raio de 10km
     // Filtrar por tecnologias
-    const {latitude, longitude, techs, exactMatch} = request.query;
+    const {latitude, longitude, techs, exactMatch, radius} = request.query;
 
-    techsArray = parseStringAsArray(techs);
+    const techsArray = parseStringAsArray(techs);
+    const radiusInMeters = radius * 1000;
 
     let devs = [];
-    for(tech of techsArray) {
+    for(const tech of techsArray) {
       let expression = "";
       
       if (exactMatch === 'true') {
@@ -29,33 +30,12 @@ module.exports = {
               type: 'Point',
               coordinates: [longitude, latitude]
             },
-            $maxDistance: 10000
+            $maxDistance: radiusInMeters
           }
         }
       });
       devs = [...devs, ...devsResult];
     }
-    // Para tornar case-insensitive
-    // salvar tudo minúsculo e pesquisar sempre minúsculo
-    // Mas não vai ser possível usar o LIKE, então ver como fazer isso
-    // Fazer um find por regex para cada tech e fazer um append ao array de devs?
-    // const devs = await Dev.find({
-    //   // Uso um objeto pois posso ter vários filtros para tecnologia
-    //   techs:{
-    //     // Pegar os devs que têm as techs EM techsArray
-    //     // $in: Operador lógico do MongoDB
-    //     $in:techsArray
-    //   },
-    //   location:{
-    //     $near:{
-    //       $geometry:{
-    //         type: 'Point',
-    //         coordinates: [longitude,latitude]
-    //       },
-    //       $maxDistance: 10000
-    //     }
-    //   }
-    // });
     
     return response.json({devs});
   }
